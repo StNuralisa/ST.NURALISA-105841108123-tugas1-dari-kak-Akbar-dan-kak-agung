28daef7c3be5b374cb6f24efe7e891c8b0fcba27
StNuralisa <150004163+StNuralisa@users.noreply.github.com>
2024-06-26 11:55:15 +0800

    Add files via upload

---
+++ tugas1.js
@@ -0,0 +1,11 @@
+console.log("hello you");
+let nama = "ST.NURALISA";
+console.log("nama :" +nama)
+let daerah = "BULUKUMBA";
+console.log("daerah :" +daerah)
+let tanggallahir = "02-07-2004";
+console.log("tanggallahir :" +tanggallahir)
+let umur ="19 tahun";
+console.log("umur :" +umur)
+let pekerjaan = "MAHASISWA";
+console.log("pekerjaan :" +pekerjaan)
